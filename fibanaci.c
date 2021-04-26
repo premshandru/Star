71d906b9d3f17a9215ac05ce90c754ff05ee2329
premshandru <premshandru.india@gmail.com>
2021-04-26 15:13:33 +0530

    Hi prem

---
+++ fibanaci.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n, f = 0, s=1, t;
+    printf("Enter the rows");
+    scanf("%d",&n);
+   
+    for (int i = 0; i<n; i++)
+    {
+        
+       
+        
+        printf("\n");
+    }
+}                    
